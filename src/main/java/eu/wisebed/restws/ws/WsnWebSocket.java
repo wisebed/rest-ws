7097c0c25a8cff1c492c8102d8b429c473c3bf7a
Daniel Bimschas <daniel@bimschas.com>
2012-01-26 18:04:47 +0100

    WebSocket demonstrator added

--- src/main/java/eu/wisebed/restws/ws/WsnWebSocket.java
+++ src/main/java/eu/wisebed/restws/ws/WsnWebSocket.java
@@ -2,12 +2,17 @@ package eu.wisebed.restws.ws;
 
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
+import de.uniluebeck.itm.tr.util.ExecutorUtils;
 import eu.wisebed.restws.WsnInstanceCache;
 import eu.wisebed.restws.util.InjectLogger;
 import org.eclipse.jetty.websocket.WebSocket;
 import org.slf4j.Logger;
 
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class WsnWebSocket implements WebSocket, WebSocket.OnTextMessage {
 
@@ -20,6 +25,17 @@ public class WsnWebSocket implements WebSocket, WebSocket.OnTextMessage {
 
 	private Connection connection;
 
+	private ScheduledExecutorService executor;
+
+	private final Runnable aliveRunnable = new Runnable() {
+		@Override
+		public void run() {
+			sendMessage("I'm alive");
+		}
+	};
+
+	private ScheduledFuture<?> aliveSchedule;
+
 	@Inject
 	public WsnWebSocket(final WsnInstanceCache wsnInstanceCache, @Assisted final String experimentId) {
 		this.wsnInstanceCache = wsnInstanceCache;
@@ -28,6 +44,10 @@ public class WsnWebSocket implements WebSocket, WebSocket.OnTextMessage {
 
 	@Override
 	public void onMessage(final String data) {
+		sendMessage(data);
+	}
+
+	private void sendMessage(final String data) {
 		try {
 			connection.sendMessage(data);
 		} catch (IOException e) {
@@ -38,6 +58,8 @@ public class WsnWebSocket implements WebSocket, WebSocket.OnTextMessage {
 	@Override
 	public void onOpen(final Connection connection) {
 		this.connection = connection;
+		this.executor = Executors.newScheduledThreadPool(1);
+		this.aliveSchedule = this.executor.scheduleAtFixedRate(aliveRunnable, 2, 2, TimeUnit.SECONDS);
 		log.info("Websocket connection opened: {}", connection);
 	}
 
@@ -48,6 +70,8 @@ public class WsnWebSocket implements WebSocket, WebSocket.OnTextMessage {
 					new Object[]{closeCode, message, connection}
 			);
 		}
+		this.aliveSchedule.cancel(true);
 		this.connection = null;
+		ExecutorUtils.shutdown(executor, 1, TimeUnit.SECONDS);
 	}
 }
