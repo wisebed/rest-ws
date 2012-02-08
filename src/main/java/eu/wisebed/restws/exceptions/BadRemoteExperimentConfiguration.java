145b6a6ff6275154825691cc297756aaa3bd6dea
Dennis Pfisterer <github@farberg.de>
2012-02-08 17:22:34 +0100

    New resource that converts a json file from a remote host to a flash request

---
+++ src/main/java/eu/wisebed/restws/exceptions/BadRemoteExperimentConfiguration.java
@@ -0,0 +1,12 @@
+package eu.wisebed.restws.exceptions;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+public class BadRemoteExperimentConfiguration extends WebApplicationException {
+	private static final long serialVersionUID = 6025480251388199505L;
+
+	public BadRemoteExperimentConfiguration(String reason, Exception cause) {
+		super(Response.status(Response.Status.BAD_REQUEST).entity(reason + "(" + cause + ")").build());
+	}
+}
